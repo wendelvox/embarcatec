4ce946e391ae0c7a466df42092ccf7608999a00a
wendelvox <104474457+wendelvox@users.noreply.github.com>
2024-12-12 22:47:45 -0300

    commit

---
+++ teste.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 0;
+
+    do 
+    {
+        printf("%d * 9 = %d\n",i,i*9);
+       i++;
+
+    }while (i<=10);
+    return 0;
+        /* code */
+    
+    
+}
